45eeba2d9f85bcd4bd7f6d9547e8cd78b4e729ea
Lucas Duete <lm.duete@gmail.com>
2018-06-15 08:33:16 -0300

    Adicionando segundo alfabeto.

--- src/main/java/io/github/lucasduete/App.java
+++ src/main/java/io/github/lucasduete/App.java
@@ -1,40 +1,54 @@
 package io.github.lucasduete;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 public class App {
 
-    private static final List<String> alphabet = Arrays.asList(
+    private static final List<String> alphabetA = Arrays.asList(
             "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","w","x","y","z"
+    );
+
+    private static final List<String> alphabetB = Arrays.asList(
+            "4","b","h","d","3","z","g","c","a","j","k","t","m","n","1", "0","p","q","r","o","s","l","u","w","x","y","f","e"
     );
 
     public static void main(String[] args) {
 
 //        Letra A
 //        System.out.printf("Letra A:\n");
-//        encryptText(10,"Hello World");
+//        encryptText(10,"Hello World",1);
 
 //        System.out.printf("\n\n");
 
 //        Letra B
 //        System.out.printf("Letra B:\n");
-//        encryptText(5,"HelloWorld");
+//        encryptText(5,"HelloWorld",1);
 
 
         System.out.printf("\n\nDigite o Texto:");
         Scanner scanner = new Scanner(System.in);
 
         decryptText(
-                scanner.next()
+                scanner.next(), 1
         );
 
     }
 
-    private static void encryptText(int key, String plainText) {
+    private static void encryptText(int key, String plainText, int choise) {
         String cifredText = plainText.toLowerCase().replaceAll(" ","");
-        System.out.printf("\n\ncifrado::: %s\n", cifredText);
+        List<String> alphabet = alphabetA;
+
+        switch (choise) {
+            case 1:
+                alphabet = alphabetA;
+                break;
+            case 2:
+                alphabet = alphabetB;
+                break;
+        }
 
         for(int i = 0; i < cifredText.length(); i++) {
             int positionNewKey = alphabet.indexOf(String.valueOf(cifredText.charAt(i))) + key;
@@ -46,8 +60,18 @@ public class App {
         }
     }
 
-    private static void decryptText(String cifredText) {
+    private static void decryptText(String cifredText, int choise) {
         String plaindText = cifredText.toLowerCase().replaceAll(" ","");
+        List<String> alphabet = alphabetA;
+
+        switch (choise) {
+            case 1:
+                alphabet = alphabetA;
+                break;
+            case 2:
+                alphabet = alphabetB;
+                break;
+        }
 
         for(int key = 0; key <= alphabet.size(); key++) {
 
